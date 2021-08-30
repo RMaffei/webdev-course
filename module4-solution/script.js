2160bd511f2d1857b244054671bf56df6a357bab
Rae Maffei <raemaffei@gmail.com>
2021-08-30 15:26:57 -0700

    Updating syntax

--- module4-solution/script.js
+++ module4-solution/script.js
@@ -39,6 +39,7 @@ var names = ["Yaakov", "John", "Jen", "Jason", "Paul", "Frank", "Larry", "Paula"
 // Loop over the names array and say either 'Hello' or "Good Bye"
 // using either the helloSpeaker's or byeSpeaker's 'speak' method.
 // See Lecture 50, part 1
+
 for (var i in names) {
 
   // STEP 11: (NOTHING TO DO. ALREADY DONE FOR YOU)
@@ -48,7 +49,7 @@ for (var i in names) {
   // string object's 'toLowerCase' method on the result so we can compare
   // to lower case character 'j' afterwards.
   // Look up these methods on Mozilla Developer Network web site if needed.
-  var firstLetter = ((names[i].charAt(0)).toLowerCase();
+  var firstLetter = ((names[i]).charAt(0)).toLowerCase();
 
   // STEP 12: (NOTHING TO DO. ALREADY DONE FOR YOU)
   // Compare the 'firstLetter' retrieved in STEP 11 to lower case
